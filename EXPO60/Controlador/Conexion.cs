1e82886a8a397598f13806ddace6bad5101d9267
William Carcamo <William Carcamo@LAPTOP-2VS969AG>
2019-08-03 20:19:29 -0600

    Agrege el Login

---
+++ EXPO60/Controlador/Conexion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace EXPO60.Controlador
+{
+    class Conexion
+    {
+        public static MySqlConnection ObtenerConexion()
+        {
+            MySqlConnection connect;
+            string server = "127.0.0.1";
+            string database = "Expo6O";
+            string user = "root";
+            string pass = "";
+            try
+            {
+                connect = new MySqlConnection("server = " + server +
+                                                ";database = " + database +
+                                                ";Uid = " + user +
+                                                ";pwd = " + pass);
+                connect.Open();
+                return connect;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error critico de conexion " + e, "Fallo de conexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connect = new MySqlConnection();
+                return connect = new MySqlConnection();
+            }
+        }
+
+        }
+}
+
+
---
+++ EXPO60/Controlador/ContructorLogin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXPO60.Controlador
+{
+    class ContructorLogin
+    {
+        public static string usuario { get; set; }
+        public string clave { get; set; }
+        public static string nombre { get; set; }
+        public static int nivel { get; set; }
+        public ContructorLogin(string usuario, string clave)
+        {
+            usuario = ContructorLogin.usuario;
+            clave = this.clave;
+        }
+
+    }
+}
