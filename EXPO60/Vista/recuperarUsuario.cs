f72497b9c585a39e29c2d6e311cc979843ecde89
Raferp <20190151@ricaldone.edu.sv>
2019-08-07 00:08:52 -0600

    validar correo

--- EXPO60/Vista/recuperarUsuario.cs
+++ EXPO60/Vista/recuperarUsuario.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using EXPO60.Controlador;
 using EXPO60.Modelo;
+using System.Text.RegularExpressions;
 
 namespace EXPO60.Vista
 {
@@ -18,11 +19,57 @@ namespace EXPO60.Vista
         {
             InitializeComponent();
         }
+        public void vaciarampos()
+        {
+            
+            txtusuarioRecuperar.Clear();
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
             validarRecuperar.resetClave(txtusuarioRecuperar.Text);
-            
+            if (Email_Valido(txtusuarioRecuperar.Text) == false)// llamado del metodo Email_Valido
+            {
+                error1.SetError(txtusuarioRecuperar, " Ingrese un Email Válido");
+                txtusuarioRecuperar.Focus();
+                vaciarampos();
+                return;
+            }
+            else
+            {
+                error1.Clear();
+                vaciarampos();
+
+            }
+
+        }
+        public static bool Email_Valido(String email) // Método para validar el Email ingresado
+        {
+            String validando;
+            validando = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+            if (Regex.IsMatch(email, validando))
+            {
+                if (Regex.Replace(email, validando, String.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private void txtusuarioRecuperar_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
