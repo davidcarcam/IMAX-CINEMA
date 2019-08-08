b0a69df1ff46facbc5870d78516c5a83fb10efe0
William Carcamo <William Carcamo@LAPTOP-2VS969AG>
2019-08-07 19:43:50 -0600

    ya esta conectada con la base de datos

--- EXPO60/Controlador/Conexion.cs
+++ EXPO60/Controlador/Conexion.cs
@@ -15,7 +15,7 @@ namespace EXPO60.Controlador
         {
             MySqlConnection connect;
             string server = "127.0.0.1";
-            string database = "cinemax";
+            string database = "expo60";
             string user = "root";
             string pass = "";
             try
