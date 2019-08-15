fd30b8ba31d813886689e7e0c1463e5e955082c5
Raferp <20190151@ricaldone.edu.sv>
2019-08-14 21:45:37 -0600

    pruebas

--- EXPO60/Vista/FrmProducto.Designer.cs
+++ EXPO60/Vista/FrmProducto.Designer.cs
@@ -271,6 +271,7 @@
             this.btnagregar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnagregar.Textcolor = System.Drawing.Color.White;
             this.btnagregar.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click_1);
             // 
             // txtprecio
             // 
